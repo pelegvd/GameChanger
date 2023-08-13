bd2e8c76bdb8df897dfdbcc1aa2acd5dbd93fc66
May Natan <81708674+maynatan@users.noreply.github.com>
2023-08-13 15:53:43 +0300

    update router file

--- app/Server.ts
+++ app/Server.ts
@@ -2,15 +2,41 @@ import express from 'express';
 var bodyParser = require('body-parser');
 var cors = require('cors');
 const mongoose = require('mongoose');
-import Event from './models/Event';
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var createError = require('http-errors');
+//import Event from './models/Event';
 
+var mongoDB = 'mongodb+srv://gamechangerdb:Jolthit2023^@cluster0.www3fos.mongodb.net/';
+mongoose.connect(mongoDB);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+
+var eventRouter = require('./routes/routes');
 
 const app = express();
+
+app.listen(9000, () => console.log(`server started on http://localhost:9000`));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(cors())
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/event', async  (req, res) => {
+
+app.use('/event', eventRouter);
+
+//app.get('/event', async  (req, res) => {
     //const events = await Event.find();
     //Event.find().then((events) => console.log(events))
     //.catch((err) => console.log(err));
@@ -18,78 +44,76 @@ app.get('/event', async  (req, res) => {
     //res.status(200);
     //res.json(events);
     //res.end();
-})
+//})
 
 
 
-app.post("/event", async (req, res) => {
-    await Event.create({
-        id : new mongoose.Types.ObjectId(),
-        title : req.body.title,
-        description : req.body.description,
-        location : req.body.location,
-    });
+//app.post("/event", async (req, res) => {
+//    await Event.create({
+//        id : new mongoose.Types.ObjectId(),
+//        title : req.body.title,
+//        description : req.body.description,
+//        location : req.body.location,
+//    });
 
-    res.status(200);
-    res.end();
-});
+//    res.status(200);
+//    res.end();
+//});
 
-app.put("/event/:id", async (req, res) => {
-    const event = new Event({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        location : req.body.location,
-      });
-      Event.updateOne({_id: req.params.id}, event).then(
-        () => {
-          res.status(201).json({
-            message: 'Thing updated successfully!'
-          });
-        }
-      ).catch(
-        (error) => {
-          res.status(400).json({
-            error: error
-          });
-        }
-      );
+//app.put("/event/:id", async (req, res) => {
+//    const event = new Event({
+//        _id: req.params.id,
+//        title: req.body.title,
+//        description: req.body.description,
+//        location : req.body.location,
+//      });
+//      Event.updateOne({_id: req.params.id}, event).then(
+//        () => {
+//          res.status(201).json({
+//            message: 'Thing updated successfully!'
+//          });
+//        }
+//      ).catch(
+//        (error) => {
+//          res.status(400).json({
+//            error: error
+//          });
+//        }
+//      );
   
-});
+//});
 
-app.delete('/event/:id', async (req, res) => {
-    Event.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-
-
-
-async function init() {
-    const mongoConnectionString = 'mongodb+srv://gamechangerdb:Jolthit2023^@cluster0.www3fos.mongodb.net/';
-
-    if (!mongoConnectionString) {
-        throw new Error("must configure mongo connection string");
-    }
-
-    await mongoose.connect(mongoConnectionString);
-
-    app.listen(9000, () => console.log(`server started on http://localhost:9000`));
-}
-
-init();
+//app.delete('/event/:id', async (req, res) => {
+//    Event.deleteOne({_id: req.params.id}).then(
+//      () => {
+//        res.status(200).json({
+//          message: 'Deleted!'
+//        });
+//      }
+//    ).catch(
+//      (error) => {
+//        res.status(400).json({
+//          error: error
+//        });
+//      }
+//    );
+//  });
 
 
 
+//async function init() {
+//    const mongoConnectionString = 'mongodb+srv://gamechangerdb:Jolthit2023^@cluster0.www3fos.mongodb.net/';
 
-//module.exports = app;
+//    if (!mongoConnectionString) {
+//        throw new Error("must configure mongo connection string");
+//    }
+
+//    await mongoose.connect(mongoConnectionString);
+
+//    app.listen(9000, () => console.log(`server started on http://localhost:9000`));
+//}
+
+//init();
+
+
+module.exports = app;
--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,22 +1,70 @@
-//const express = require('express');
-//const Event = require('../models/Event');
-//const router = express.Router();
+import express from 'express';
+var router = express.Router();
+const mongoose = require('mongoose');
+import Event from '../models/Event';
 
-//router.get('/event', async  (req, res) => {
-//    const events = await Event.find();
-//    res.send(events);
-//})
 
-//router.post('/event', async (req, res) => {
-//   const event = new Event({
-//        _id: new mongoose.Types.ObjectId(),
-//       title:req.body.title,
-//        description:req.body.decription,
-//        location:req.body.location,
-//    })
-//    await event.save();
-//    res.send(event);
-//    console.log(event);
-//})
 
-//module.exports = router;
+router.get('/event', async  (req, res) => {
+    //const events = await Event.find();
+    //Event.find().then((events) => console.log(events))
+    //.catch((err) => console.log(err));
+    //res.send(events);
+    //res.status(200);
+    //res.json(events);
+    //res.end();
+})
+
+
+router.post("/event", async (req, res) => {
+    await Event.create({
+        id : new mongoose.Types.ObjectId(),
+        title : req.body.title,
+        description : req.body.description,
+        location : req.body.location,
+    });
+
+    res.status(200);
+    res.end();
+});
+
+router.put("/event/:id", async (req, res) => {
+    const event = new Event({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        location : req.body.location,
+      });
+      Event.updateOne({_id: req.params.id}, event).then(
+        () => {
+          res.status(201).json({
+            message: 'Thing updated successfully!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
+  
+});
+
+router.delete('/event/:id', async (req, res) => {
+    Event.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Deleted!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  });
+
+  module.exports = router;
