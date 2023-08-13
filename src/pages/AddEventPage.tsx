9165f2de86868a5a70d52c3fe9af8b10d0a49d1c
May Natan <81708674+maynatan@users.noreply.github.com>
2023-08-13 23:24:24 +0300

    change ui add events

--- src/pages/AddEventPage.tsx
+++ src/pages/AddEventPage.tsx
@@ -1,37 +1,100 @@
-import React from 'react';
-import '../App.css';
-import '../stylesheets/Input.css';
-import '../stylesheets/Button.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Stack from "@mui/material/Stack";
+import "../App.css";
+import "../stylesheets/Input.css";
+import "../stylesheets/Button.css";
 
 function AddEvent() {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
-    return (
+  const defaultProps = {
+    options: categorys,
+    getOptionLabel: (option: FilmOptionType) => option.title,
+  };
+  const flatProps = {
+    options: categorys.map((option) => option.title),
+  };
+  const [value, setValue] = React.useState<FilmOptionType | null>(null);
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div>
-                    <label>Event Name</label>
-                    <input name='eventName'
-                        placeholder='name' />
-                </div>
-                <div>
-                    <label>Description</label>
-                    <input name='description'
-                        placeholder='description' />
-                </div>
-                <div>
-                    <label>Location</label>
-                    <input name='location'
-                        placeholder='location' />
-                </div>
-                <div>
-                    <button className='btn-submit' type='submit'>Add</button>
-                </div>
-            </form>
+          <label>Event Name</label>
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Event Name"
+          />
         </div>
-    );
+        <div>
+          <label>Description</label>
+          <TextField
+            id="outlined-multiline-static"
+            fullWidth
+            multiline
+            rows={9}
+            defaultValue="Description"
+          />
+        </div>
+        <div>
+          <label>Location</label>
+          <input name="location" placeholder="location" />
+        </div>
+
+        <Stack>
+          <Autocomplete
+            {...defaultProps}
+            id="clear-on-escape"
+            clearOnEscape
+            includeInputInList
+            size="small"
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Choose Category"
+                variant="standard"
+              />
+            )}
+          />
+        </Stack>
+
+        <div>
+          <TextField
+            id="outlined-number"
+            label="Number"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <div>
+          <button className="btn-submit" type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
+interface FilmOptionType {
+  title: string;
+}
+
+const categorys = [
+  { title: "Sport" },
+  { title: "Art" },
+  { title: "Volenteer" },
+  { title: "Food" },
+  { title: "Watch" },
+];
 
 export default AddEvent;
