dcda130024dc94a851869e761c2e2d517f89c2d0
Peleg Vadbeker <104712912+pelegvd@users.noreply.github.com>
2023-09-26 19:31:31 +0300

    changes footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,79 @@
-import React from "react";
-import "./footer.css";
-function Footer() {
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+export default function Footer() {
     return (
-        <footer>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h3>About Us</h3>
-                        <p>We are a web development company that specializes in building React applications.</p>
-                    </div>
-                    <div className="col-md-4">
-                        <h3>Contact Us</h3>
-                        <p>
-                            <a href="mailto:hello@example.com">hello@example.com</a>
-                        </p>
-                    </div>
-                    <div className="col-md-4">
-                        <h3>Social Media</h3>
-                        <ul className="social-media-links">
-                            <li>
-                                <a href="https://www.facebook.com/example">
-                                    <i className="fab fa-facebook-f"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.twitter.com/example">
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/example">
-                                    <i className="fab fa-instagram"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <p className="copyright">Copyright &copy; 2023 Example Company</p>
-        </footer>
+        <Box
+            component="footer"
+            sx={{
+                backgroundColor: (theme) =>
+                    theme.palette.mode === "light"
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[800],
+                p: 6,
+            }}
+        >
+            <Container maxWidth="lg">
+                <Grid container spacing={5}>
+                    <Grid item xs={12} sm={4}>
+                        <Typography variant="h6" color="text.primary" gutterBottom>
+                            About Us
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+
+                            GameChanger is a platform that helps people connect with each other and play sports, such as football, basketball, and more.
+                            It also connects people who want to volunteer with companies that need volunteers.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Typography variant="h6" color="text.primary" gutterBottom>
+                            Contact Us
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Shvil Hameretz 2 Tel Aviv, Israel
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Email: GameChanger@gmail.com
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Phone: +1 234 567 8901
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Typography variant="h6" color="text.primary" gutterBottom>
+                            Follow Us
+                        </Typography>
+                        <Link href="https://www.facebook.com/" color="inherit">
+                            <Facebook />
+                        </Link>
+                        <Link
+                            href="https://www.instagram.com/"
+                            color="inherit"
+                            sx={{ pl: 1, pr: 1 }}
+                        >
+                            <Instagram />
+                        </Link>
+                        <Link href="https://www.twitter.com/" color="inherit">
+                            <Twitter />
+                        </Link>
+                    </Grid>
+                </Grid>
+                <Box mt={5}>
+                    <Typography variant="body2" color="text.secondary" align="center">
+                        {"Copyright © "}
+                        <Link color="inherit" href="https://your-website.com/">
+                            GameChanger
+                        </Link>{" "}
+                        {new Date().getFullYear()}
+                        {"."}
+                    </Typography>
+                </Box>
+            </Container>
+        </Box>
     );
 }
-
-export default Footer;
