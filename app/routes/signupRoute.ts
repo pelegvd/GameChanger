dbc38d303136779fd0fcdefa7717954854930d17
May Natan <81708674+maynatan@users.noreply.github.com>
2023-10-05 09:00:52 +0300

    user route

--- app/routes/signupRoute.ts
+++ app/routes/signupRoute.ts
@@ -39,58 +39,106 @@ signupRouter.get("/:id", async (req, res) => {
 });
 
 signupRouter.post("/", async (req, res) => {
-  const password = PWD_TOOL.calculatesHmacAndSalt(req.body.password);
-  const passwordHash = password.hmac;
-  const passwordSalt = password.salt;
-  var schema = new passwordValidator();
-  User.findOne({ email: req.body.email })
-    .exec()
-    .then((doc) => {
-      if (doc) {
-        console.log(doc);
-        res.status(500).send("ERROR: User with this Email Already Exist");
-      } else {
-        if (validator.isEmail(req.body.email)) {
-          if (
-            validator.isStrongPassword(req.body.password, {
-              minLength: 10,
-              minLowercase: 1,
-              minUppercase: 1,
-              minNumbers: 1,
-              minSymbols: 1,
-            }) &&
-            schema.is().not().oneOf(["Passw0rd", "Aa12345678!", "P@ssw0rd123"])
-          ) {
-            saveUser();
-            // CreateHistory();
-          } else {
-            return res.status(500).send("ERROR:Week Password");
-          }
-        } else {
-          return res.status(500).send("ERROR:Email Is Not Correct");
+
+  if(req.body.username && req.body.password && req.body.emailAddress && req.body.firstName && req.body.lastName){
+    results = databaseConnection.query(`SELECT userName FROM users WHERE userName = '${req.body.username}'`)
+    if(results.length == 0){
+        // password validation:
+        if(validator.isStrongPassword(req.body.password, {
+                         minLength: 10,
+                         minLowercase: 1,
+                        minUppercase: 1,
+                         minNumbers: 1,
+                         minSymbols: 1,
+        })){
+            var validationMessage = passwordValidation.error.details[0].message
+            res.status(400).send(`ValidationErr: ${validationMessage}`)
+        }else{
+          const password = PWD_TOOL.calculatesHmacAndSalt(req.body.password);
+          const passwordHash = password.hmac;
+          const passwordSalt = password.salt;
+          User.create({
+            id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: passwordHash,
+            salt: passwordSalt,
+            type: req.body.type,
+          });
         }
-      }
-    });
+    }else{
+        
+        res.status(400).send(`the user '${req.body.username}' is already exists` + JSON.stringify(results));
+    }
+} else{
+    res.status(400).send("One or more parameters are not provided. Required parameters:'username','password','emailAddress'");
+}
 
-  const user = new User({
-    id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    password: passwordHash,
-    salt: passwordSalt,
-    type: req.body.type,
-  });
 
-  function saveUser() {
-    user
-      .save()
-      .then((result) => {
-        console.log(result);
-        res.status(200).redirect("/");
-      })
-      .catch((err) => {
-        res.status(500).send("ERROR: User Already Exist");
-      });
-  }
+  
+
+  User.findOne({ email: req.body.email }).exec().then(doc) =>
+
+  await 
+
+  res.status(200);
+  res.end();
 });
+
+// signupRouter.post("/", async (req, res) => {
+//   const password = PWD_TOOL.calculatesHmacAndSalt(req.body.password);
+//   const passwordHash = password.hmac;
+//   const passwordSalt = password.salt;
+
+//   User.findOne({ email: req.body.email })
+//     .exec()
+//     .then((doc) => {
+//       if (doc) {
+//         console.log(doc);
+//         res.status(500).send("ERROR: User with this Email Already Exist");
+//       } else {
+//         if (validator.isEmail(req.body.email)) {
+//           if (
+//             validator.isStrongPassword(req.body.password, {
+//               minLength: 10,
+//               minLowercase: 1,
+//               minUppercase: 1,
+//               minNumbers: 1,
+//               minSymbols: 1,
+//             })
+//           ) {
+//             saveUser();
+//             // CreateHistory();
+//           } else {
+//             return res.status(500).send("ERROR:Week Password");
+//           }
+//         } else {
+//           return res.status(500).send("ERROR:Email Is Not Correct");
+//         }
+//       }
+//     });
+
+//   const user = new User({
+//     id: new mongoose.Types.ObjectId(),
+//     name: req.body.name,
+//     email: req.body.email,
+//     phone: req.body.phone,
+//     password: passwordHash,
+//     salt: passwordSalt,
+//     type: req.body.type,
+//   });
+
+//   function saveUser() {
+//     user
+//       .save()
+//       .then((result) => {
+//         console.log(result);
+//         res.status(200).redirect("/");
+//       })
+//       .catch((err) => {
+//         res.status(500).send("ERROR: User Already Exist");
+//         console.log(res);
+//       });
+//   }
+// });
--- src/api/SignupService.ts
+++ src/api/SignupService.ts
@@ -3,5 +3,6 @@ import instanceAxios from "./http-common";
 const url = "/signup";
 
 export const signupPost = (data: any) => {
+  console.log(data);
   return instanceAxios.post(url, data);
 };
